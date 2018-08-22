e667c1f3be2d880205b56ca611a3252af6f968a7
AyAmeng <372969383@qq.com>
2018-08-22 11:03:37 +0800

    feat(cookie middleware): 增加新的 cookie middleware

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const Koa = require('koa')
 const router = require('./routers/index')
 const loggerMiddleWare = require('./middleware/logger-async')
+const cookieMiddleWare = require('./middleware/cookie')
 
 const app = new Koa()
 
 app.use(loggerMiddleWare())
+
+app.use(cookieMiddleWare())
 
 app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(8080, () => {
   console.info('☀️ ️ => app is running at http://localhost:8080')
 })
+getSnapshotBeforeUpdate = (prevProps, prevState) => {
+  
+}
